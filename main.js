ffd889d4e1c23e4a69c980de7e48e241f402b53e
csgmbz <csgmbz@gmail.com>
2026-03-28 15:37:09 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Add JS here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const targetType = document.getElementById('target_type');
+    const pName = document.getElementById('p_name');
+    const pMsg = document.getElementById('p_msg');
+
+    targetType.disabled = false;
+    pName.disabled = false;
+    pMsg.disabled = false;
+});
